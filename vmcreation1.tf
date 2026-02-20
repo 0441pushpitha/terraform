9859b3ca3838a6d43103f6adc894eedf7b95a5a7
mytestgit <bondilipushpithabai@gmail.com>
2026-02-20 16:06:51 +0530

    new file created for vm creation

---
+++ vmcreation1.tf
@@ -0,0 +1,25 @@
+  provider "google" {
+  project = "107024447502780705397"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "my_vm" {
+  name         = "pushpitha-test vm creation from GCP "   # 👈 VM Name goes here
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+}
+
